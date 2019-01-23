5c25f713575a25ca52d94ce3a7ab3038c8da6706
nkg <devnkjm@gmail.com>
2019-01-23 20:17:13 +0900

    update spider

--- event_notify/spiders/eventsite.py
+++ event_notify/spiders/eventsite.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin
 import scrapy
 
 
@@ -6,6 +7,13 @@ class EventsiteSpider(scrapy.Spider):
     name = "eventsite"
     allowed_domains = [os.getenv("ALLOWED_DOMAINS")]
     start_urls = [os.getenv("START_URLS")]
+    root_url = os.getenv("ROOT_URL")
 
     def parse(self, response):
+        for url in response.css(
+            "header.c-eventSummary-header > a::attr('href')"
+        ).extract():
+            yield scrapy.Request(urljoin(self.root_url, url), self.parse_event)
+
+    def parse_event(self, response):
         pass
